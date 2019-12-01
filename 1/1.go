e984b359668dd49cebab362acec155ae34816665
oskar <oskar.c.johansson@skf.com>
2019-12-01 22:14:19 +0100

    day1

---
+++ 1/1.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	acc := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		i, _ := strconv.Atoi(line)
+		res := (i / 3) - 2
+		acc = acc + res
+		for res > 0 && res > 3 {
+			res = (res / 3) - 2
+			if res > 0 {
+				acc = acc + res
+			}
+
+		}
+	}
+	fmt.Println(acc)
+
+}
