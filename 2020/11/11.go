d2514f86740e74184f06de471e51e7f66b2dcd52
Oskar Johansson <oskar.c.johansson@skf.com>
2020-12-12 00:39:04 +0100

    ???

--- 2020/11/11.go
+++ 2020/11/11.go
@@ -87,19 +87,20 @@ func round(seats map[string]Seat, xMax int, yMax int) (result map[string]Seat, c
 	for i := 0; i < xMax; i++ {
 		for j := 0; j < yMax; j++ {
 			key := strconv.Itoa(j) + "-" + strconv.Itoa(i)
-			getVisibleSeats(key, seats)
-			fmt.Println(key)
+			visi := getVisibleSeats(key, seats)
+			//	fmt.Println(visi)
 			switch seats[key].val {
 			case "L":
-
 				canSit := true
-				for _, k := range seats[key].adjList {
+
+				for _, k := range visi {
 					if _, ok := seats[k]; ok {
 						if seats[k].val == "#" {
 							canSit = false
 						}
 					}
 				}
+
 				if canSit {
 					changes++
 					seatChange := seats[key]
@@ -108,17 +109,18 @@ func round(seats map[string]Seat, xMax int, yMax int) (result map[string]Seat, c
 				} else {
 					result[key] = seats[key]
 				}
-
 			case "#":
 				seatsBusy := 0
-				for _, k := range seats[key].adjList {
+
+				for _, k := range visi {
 					if _, ok := seats[k]; ok {
 						if seats[k].val == "#" {
 							seatsBusy++
 						}
 					}
 				}
-				if 4 <= seatsBusy {
+
+				if 5 <= seatsBusy {
 					changes++
 					seatChange := seats[key]
 					seatChange.val = "L"
@@ -136,32 +138,126 @@ func round(seats map[string]Seat, xMax int, yMax int) (result map[string]Seat, c
 	return
 }
 
-func getVisibleSeats(key string, seats map[string]Seat) {
-
+func getVisibleSeats(key string, seats map[string]Seat) (lookers []string) {
 	distance := 1
-
 	foundKey1 := false
-
 	for !foundKey1 {
 		key1 := strconv.Itoa(seats[key].x-distance) + "-" + strconv.Itoa(seats[key].y-distance)
 		if _, ok := seats[key1]; ok {
 			if seats[key1].val != "." {
 				foundKey1 = true
-				fmt.Println(key1)
+				lookers = append(lookers, key1)
 			}
 		} else {
 			foundKey1 = true
-			fmt.Println("nokey")
 		}
+		distance = distance + 1
 	}
-	fmt.Println("leave")
-	/*
-		key2 := strconv.Itoa(xlocal-1) + "-" + strconv.Itoa(ylocal)
-		key3 := strconv.Itoa(xlocal-1) + "-" + strconv.Itoa(ylocal+1)
-		key4 := strconv.Itoa(xlocal) + "-" + strconv.Itoa(ylocal-1)
-		key5 := strconv.Itoa(xlocal) + "-" + strconv.Itoa(ylocal+1)
-		key6 := strconv.Itoa(xlocal+1) + "-" + strconv.Itoa(ylocal-1)
-		key7 := strconv.Itoa(xlocal+1) + "-" + strconv.Itoa(ylocal)
-		key8 := strconv.Itoa(xlocal+1) + "-" + strconv.Itoa(ylocal+1)
-	*/
+
+	distance = 1
+	foundKey2 := false
+	for !foundKey2 {
+		key2 := strconv.Itoa(seats[key].x-distance) + "-" + strconv.Itoa(seats[key].y)
+		if _, ok := seats[key2]; ok {
+			if seats[key2].val != "." {
+				foundKey2 = true
+				lookers = append(lookers, key2)
+			}
+		} else {
+			foundKey2 = true
+		}
+		distance = distance + 1
+	}
+
+	distance = 1
+	foundKey3 := false
+	for !foundKey3 {
+		key3 := strconv.Itoa(seats[key].x-distance) + "-" + strconv.Itoa(seats[key].y+distance)
+		if _, ok := seats[key3]; ok {
+			if seats[key3].val != "." {
+				foundKey3 = true
+				lookers = append(lookers, key3)
+			}
+		} else {
+			foundKey3 = true
+		}
+		distance = distance + 1
+	}
+
+	distance = 1
+	foundKey4 := false
+	for !foundKey4 {
+		key4 := strconv.Itoa(seats[key].x) + "-" + strconv.Itoa(seats[key].y-distance)
+		if _, ok := seats[key4]; ok {
+			if seats[key4].val != "." {
+				foundKey4 = true
+				lookers = append(lookers, key4)
+			}
+		} else {
+			foundKey4 = true
+		}
+		distance = distance + 1
+	}
+
+	distance = 1
+	foundKey5 := false
+	for !foundKey5 {
+		key5 := strconv.Itoa(seats[key].x) + "-" + strconv.Itoa(seats[key].y+distance)
+		if _, ok := seats[key5]; ok {
+			if seats[key5].val != "." {
+				foundKey5 = true
+				lookers = append(lookers, key5)
+			}
+		} else {
+			foundKey5 = true
+		}
+		distance = distance + 1
+	}
+
+	distance = 1
+	foundKey6 := false
+	for !foundKey6 {
+		key6 := strconv.Itoa(seats[key].x+distance) + "-" + strconv.Itoa(seats[key].y-distance)
+		if _, ok := seats[key6]; ok {
+			if seats[key6].val != "." {
+				foundKey6 = true
+				lookers = append(lookers, key6)
+			}
+		} else {
+			foundKey6 = true
+		}
+		distance = distance + 1
+	}
+
+	distance = 1
+	foundKey7 := false
+	for !foundKey7 {
+		key7 := strconv.Itoa(seats[key].x+distance) + "-" + strconv.Itoa(seats[key].y)
+		if _, ok := seats[key7]; ok {
+			if seats[key7].val != "." {
+				foundKey7 = true
+				lookers = append(lookers, key7)
+			}
+		} else {
+			foundKey7 = true
+		}
+		distance = distance + 1
+	}
+
+	distance = 1
+	foundKey8 := false
+	for !foundKey8 {
+		key8 := strconv.Itoa(seats[key].x+distance) + "-" + strconv.Itoa(seats[key].y+distance)
+		if _, ok := seats[key8]; ok {
+			if seats[key8].val != "." {
+				foundKey8 = true
+				lookers = append(lookers, key8)
+			}
+		} else {
+			foundKey8 = true
+		}
+		distance = distance + 1
+	}
+	return
+
 }
