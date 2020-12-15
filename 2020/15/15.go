8f8d847c124b800cc513880670a7ae0e6ff3d3b1
Oskar Johansson <oskar.c.johansson@skf.com>
2020-12-15 17:33:50 +0100

    15

---
+++ 2020/15/15.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+type Number struct {
+	lastSpoken  []int
+	timesSpoken int
+}
+
+func main() {
+	//scanner := bufio.NewScanner(os.Stdin)
+
+	//input := []int{0, 5, 4, 1, 10, 14, 7}
+	gameMap := make(map[int]*Number)
+	input := []int{1, 3, 2}
+	n1 := Number{lastSpoken: []int{1}, timesSpoken: 1}
+	gameMap[1] = &n1
+	n2 := Number{lastSpoken: []int{2}, timesSpoken: 1}
+	gameMap[3] = &n2
+	n3 := Number{lastSpoken: []int{3}, timesSpoken: 1}
+	gameMap[2] = &n3
+
+	//input := []int{0, 5, 4, 1, 10, 14, 7}
+	i := 4
+	lastSpok := 2
+	for {
+		fmt.Println(i, lastSpok)
+		if _, ok := gameMap[lastSpok]; ok {
+
+			newVal := gameMap[lastSpok].lastSpoken[gameMap[lastSpok].timesSpoken-1] -
+				gameMap[lastSpok].lastSpoken[gameMap[lastSpok].timesSpoken-1]
+
+			if newV, ok := gameMap[lastSpok]; ok {
+				newV.lastSpoken = append(gameMap[newVal].lastSpoken, i)
+				newV.timesSpoken = len(gameMap[newVal].lastSpoken)
+			} else {
+				nx := Number{lastSpoken: []int{i}, timesSpoken: 1}
+				gameMap[newVal] = &nx
+			}
+			input = append(input, newVal)
+			lastSpok = newVal
+		} else {
+
+			if _, ok := gameMap[0]; ok {
+				nx := Number{lastSpoken: []int{0}, timesSpoken: 1}
+
+				gameMap[0] = &nx
+
+			} else {
+				gameMap[0].lastSpoken = append(gameMap[0].lastSpoken, i)
+				gameMap[0].timesSpoken = len(gameMap[0].lastSpoken)
+
+			}
+			input = append(input, 0)
+
+			lastSpok = 0
+		}
+		i++
+		if i == 2020 {
+			break
+		}
+	}
+	fmt.Println(input)
+}
