d62b350a4e2be89b83ba122f580b3dc023d883b1
Oskar Johansson <oskar.c.johansson@skf.com>
2020-12-19 00:25:51 +0100

    asd

--- 2020/18/18.go
+++ 2020/18/18.go
@@ -19,8 +19,56 @@ type key struct {
 }
 
 func main() {
+
 	scanner := bufio.NewScanner(os.Stdin)
 	tot := 0
+	/*
+		var plussings []int
+
+		var q []string
+		for scanner.Scan() {
+			line := scanner.Text()
+			q = strings.Split(line, " ")
+			plussing := 0
+			for c, d := range q {
+				if string(d[0]) == "(" {
+					plussing++
+					plussings = append(plussings, c)
+				} else if string(d[len(d)-1]) == ")" {
+					plussing--
+				}
+
+				if d == "+" {
+
+					q[c-1] = "(" + q[c-1]
+					plussing++
+					plussings = append(plussings, c)
+					if plussing > 0 && len(q)-2 == c {
+						q[c+1] = q[c+1] + ")"
+						plussing--
+					} else {
+
+					}
+				} else if d == "*" && plussing > 0 {
+					q[c-1] = q[c-1] + ")"
+					plussing--
+
+				}
+			}
+
+			z := ""
+			for i, o := range q {
+				if i == 0 {
+					z = o
+				} else {
+					z = z + " " + o
+				}
+
+			}
+			fmt.Println(z)
+
+		}
+	*/
 	for scanner.Scan() {
 		var leftOpens []int
 		var parat []Pair
@@ -45,27 +93,42 @@ func main() {
 		}
 		var keys []key
 		for _, a := range parat {
-			z, _ := evalParent(k[a.start:a.end])
+			z := evalExpr(k[a.start:a.end])
 			reps[k[a.start:a.end]] = z
 			keys = append(keys, key{key: k[a.start:a.end], val: z})
 		}
-		for _, i := range keys {
-			for _, r := range keys {
-				if i.key != r.key {
-					if strings.Contains(r.key, i.key) {
-						nn, _ := evalParent(strings.Replace(r.key, i.key, strconv.Itoa(i.val), -1))
-						reps[r.key] = nn
-						delete(reps, i.key)
+		z := 0
 
+		for {
+			for _, i := range keys {
+				tempKey := i.key
+
+				for _, r := range keys {
+					if i.key != r.key {
+						if strings.Contains(tempKey, r.key) {
+							tempKey = strings.Replace(tempKey, r.key, strconv.Itoa(r.val), -1)
+							nn := evalExpr(tempKey)
+							reps[i.key] = nn
+							//	fmt.Println(tempKey, r.key, r.val)
+							delete(reps, r.key)
+
+						}
 					}
 				}
 			}
+			z++
+			//fmt.Println(z)
+			if z > 20 {
+				break
+			}
 		}
+		//fmt.Println(reps)
+
 		a := line
 		for i, k := range reps {
 			a = strings.Replace(a, i, strconv.Itoa(k), -1)
 		}
-		loc, _ := evalParent(a)
+		loc := evalExpr(a)
 		tot = tot + loc
 		fmt.Println(a, loc)
 	}
@@ -73,7 +136,7 @@ func main() {
 
 }
 
-func evalParent(line string) (tot int, leng int) {
+func evalExpr(line string) (tot int) {
 	s := strings.Split(line, " ")
 	tot, _ = strconv.Atoi(strings.Replace(s[0], "(", "", -1))
 	var unescp []string
@@ -85,15 +148,25 @@ func evalParent(line string) (tot int, leng int) {
 	for i := 1; i < len(unescp)-1; i++ {
 
 		if s[i] == "+" {
-			n, _ := strconv.Atoi(unescp[i+1])
+			n, err := strconv.Atoi(unescp[i+1])
 			tot = tot + n
-		} else {
-			n, _ := strconv.Atoi(unescp[i+1])
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else if s[i] == "*" {
+			n, err := strconv.Atoi(unescp[i+1])
 			tot = tot * n
+			if err != nil {
+				fmt.Println(err)
+			}
+
+		} else {
+			fmt.Println("WTFF", line)
 		}
 		i++
-		leng = i
 
 	}
+	//fmt.Println(line, " == ", tot)
+
 	return
 }
