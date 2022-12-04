dc387545bfa31b9e9fcfffca313b541d828e7329
oskar <sjosidan@gmail.com>
2022-12-04 18:33:01 +0800

    restructured input files, moved R files to a new Jupyter notebook

--- 2022/R/01/01.r
+++
@@ -1,9 +0,0 @@
-setwd("C:/Users/sjosi/CODE/advent/2022/R/01")
-my_data <- read.csv("01.csv", sep=';')
-sorted = rowSums(my_data, na.rm=TRUE)
-taskA = max(sorted)
-n <- nrow(my_data)
-taskB <- sort(sorted,partial=n-1)[n-1] + sort(sorted,partial=n-2)[n-2] + taskA
-taskA
-taskB
-
--- 2022/R/02/02.r
+++
@@ -1,6 +0,0 @@
-setwd("C:/Users/sjosi/CODE/advent/2022/R/02")
-my_data <- read.csv("02.csv", sep=';')
-task02a <- sum(rowSums(my_data[ , c(3,4)], na.rm=TRUE))
-task02b <- sum(rowSums(my_data[ , c(5,6)], na.rm=TRUE))
-task02a
-task02b
